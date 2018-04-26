853df64396a32a4ccaa6020ba79118e844796923
tamakihime <37037753+tamakihime@users.noreply.github.com>
2018-04-27 01:32:10 +0900

    python ディクショナリクラス　変更

--- Dictionary.py
+++ Dictionary.py
@@ -14,6 +14,36 @@ class Dictionary:
             str = line.rstrip('\n')
             if str != '':
                 self.random.append(str)
+
+        # 破道辞書格納ようのインスタンス構築
+        self.hadou = []
+        # 破道辞書をオープン
+        hfile = open('dictionary/hadou.txt', 'r', encoding='utf_8')
+        # 各行を要素とするリストを取得
+        h_lines = hfile.readlines()
+        hfile.close()
+        # 末尾の改行と空白文字を取り除いてインスタンス変数に格納
+        self.hadou=[]
+        for line in h_lines:
+            str = line.rstrip('\n')
+            if str != '':
+                self.hadou.append(str)
+
+
+        # ランダム辞書格納ようのインスタンス構築
+        self.noguti = []
+        # ランダム辞書をオープン
+        nfile = open('dictionary/Noguti.txt', 'r', encoding='utf_8')
+        # 各行を用佐とするリストを取得
+        n_lines = nfile.readlines()
+        nfile.close()
+        # 末尾の改行と空白文字を取り除いてインスタンス変数に格納
+        self.noguti=[]
+        for line in n_lines:
+            str = line.rstrip('\n')
+            if str != '':
+                self.noguti.append(str)
+
         # パターン辞書オープン
         pfile = open('dictionary/pattern.txt', 'r', encoding='utf_8')
         # 各行を要素としてリストに格納
@@ -23,15 +53,15 @@ class Dictionary:
         # インスタンス変数（リスト）に格納
         self.new_lines = []
         for line in p_lines:
-            str = line.rstirp('\n')
+            str = line.rstrip('\n')
             if str != '':
                 self.new_lines.append(str)
         # 辞書型のインスタンス変数を用意
-        self.pattern = {}
+        self.pattern = []
         # 一行をタブで切り分けて辞書オブジェクトに格納
         # ’pattern’キー　：正規表現のパターン
         # 'phrases'キー　:応答列
         for line in self.new_lines:
             ptn, prs = line.split('\t')
             self.pattern.setdefault('pattern', []).append(ptn)
-            self.pattern.setdefault('phrase', []).append(prs)
+            self.pattern.setdefault('phrases', []).append(prs)
--- responder.py
+++ responder.py
@@ -4,20 +4,28 @@ import random
 class Responder:
     """応答クラス
     """
-    def __init__(self, name):
+    def __init__(self, name, Dictionary):
         """Responderオブジェクトの名前をnameに格納
 
             @param name Responder オブジェクトの名前
+            @:param dictiornary オブジェクトごそっと代入
         """
         self.name = name
+        self.dictionary = Dictionary
 
     def response(self, input):
         """応答文字列を作って返す
-
+            常にオーナーライドされる　だって返り値ないもの
             @param input 入力された文字列　
         """
         return ''
 
+    def get_name(self):
+        """
+        応答オブジェクトの名前を返す
+        :return:
+        """
+        return self.name
 
 class RepeatResponder(Responder):
     """オウム返しのためのサブクラス
@@ -32,79 +40,22 @@ class RepeatResponder(Responder):
 class RandomResponder(Responder):
     """ランダムな応答のためのサブクラス
     """
-    def __init__(self, name):
-        """スーパークラスの__init__（）を呼び出す
-
-
-        """
-        super().__init__(name)
-        # ランダム辞書格納ようのインスタンス構築
-        self.responses = []
-        # ランダム辞書をオープン
-        rfile = open('dictionary/random.txt', 'r', encoding='utf_8')
-        # 各行を用佐とするリストを取得
-        r_lines = rfile.readlines()
-        rfile.close()
-        # 末尾の改行と空白文字を取り除いてインスタンス変数に格納
-        for line in r_lines:
-            str = line.rstrip('\n')
-            if(str!=''):
-                self.responses.append(str)
-
     def response(self, input):
 
-        return random.choice(self.responses)
+        return random.choice(self.dictionary.random)
 
 
 class HasdouResponder(Responder):
     """破道を返すためのサブクラス
     """
-    def __init__(self, name):
-        """スーパークラスの__init__（）を呼び出す
-
-
-        """
-        super().__init__(name)
-        # ランダム辞書格納ようのインスタンス構築
-        self.responses = []
-        # ランダム辞書をオープン
-        rfile = open('dictionary/hadou.txt', 'r', encoding='utf_8')
-        # 各行を用佐とするリストを取得
-        r_lines = rfile.readlines()
-        rfile.close()
-        # 末尾の改行と空白文字を取り除いてインスタンス変数に格納
-        for line in r_lines:
-            str = line.rstrip('\n')
-            if(str!=''):
-                self.responses.append(str)
-
     def response(self, input):
 
-        return random.choice(self.responses)
+        return random.choice(self.dictionary.hadou)
 
 class NogutiResponder(Responder):
     """野口くん語録を返すためのサブクラス
     """
-    def __init__(self, name):
-        """スーパークラスの__init__（）を呼び出す
-
-
-        """
-        super().__init__(name)
-        # ランダム辞書格納ようのインスタンス構築
-        self.responses = []
-        # ランダム辞書をオープン
-        rfile = open('dictionary/Noguti.txt', 'r', encoding='utf_8')
-        # 各行を用佐とするリストを取得
-        r_lines = rfile.readlines()
-        rfile.close()
-        # 末尾の改行と空白文字を取り除いてインスタンス変数に格納
-        for line in r_lines:
-            str = line.rstrip('\n')
-            if(str!=''):
-                self.responses.append(str)
-
     def response(self, input):
 
-        return random.choice(self.responses)
+        return random.choice(self.dictionary.noguti)
 
